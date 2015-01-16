5b1d82209a1de4d8becefdbae8df55cfa427b38c
Robin Kearney <robin@kearney.co.uk>
2015-01-16 10:08:13 +0000

    Tidying of the code and config

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
-host = 'trin.riviera.org.uk';
+// Set to the hostname of your websockets enabled MQTT broker
+host = '';
+// Set to the port of your MQTT broker
 port = 9010;
-topic = '#';
+
+// Topic to subscribe to for the list of switches
+switchTopic = 'rooms/switches';
+// Topic to publish button actions
+toggleTopic = "rooms/toggle";
+
+// Leave uncommented to not use authentication to the broker
+// username = 'username";
+// password = 'password";
+
+// Not tested with TLS! WMMV.
 useTLS = false;
-// username = null;
-// password = null;
-username = 'js_client';
-password = '772CI7nQUIhSNSG';
+
+// Safe to leave
 cleansession = true;
